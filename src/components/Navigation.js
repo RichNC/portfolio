43d50807d336feccfcaf414ec6fbccba157152cf
Rich Cherry <rich.m.cherry@gmail.com>
2020-09-05 19:51:53 -0600

    Consolidated js functions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,8 +10,8 @@ export function Navigation(){
         <Link to="/projects" className="item projects" id="nav-link" onClick={screenWiden} onload={screenWiden}>Projects</Link>
         <Link to="/articles" className="item articles" onClick={lightsOff}>Articles</Link>
         <Link to="/about" className="item about" onClick={lightsOn}>About</Link>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/rich-cherry"><i class="fab fa-github"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rich-cherry/"><i class="fab fa-linkedin"></i></a>
+        <a className="item" target="_blank" rel="noopener noreferrer" href="https://github.com/rich-cherry"><i class="fab fa-github"></i></a>
+        <a className="item" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rich-cherry/"><i class="fab fa-linkedin"></i></a>
       </div>
     </div>
   )
@@ -35,29 +35,35 @@ const screenWiden = (e) => {
   textLight();
 }
 
-const textLight = () => {
-  document.body.style.color = '#F9E7E7';
+const backgroundLight = () => {
+  document.body.style.backgroundColor = '#F9E7E7';
 }
 
 const textDark = () => {
   document.body.style.color = '#12191C';
 }
 
-const screenNarrow = () => {
-  document.querySelector(".main-container").style.width = "70%";
-}
-
 const screenWide = () => {
   document.querySelector(".main-container").style.width = "90%";
 }
 
-const backgroundLight = () => {
-  document.body.style.backgroundColor = '#F9E7E7';
+const backgroundDark = () => {
+  document.body.style.backgroundColor = "";
 }
 
-const backgroundDark = () => {
-  document.body.style.backgroundColor = '#12191C';
+const textLight = () => {
+  document.body.style.color = "";
 }
+
+const screenNarrow = () => {
+  document.querySelector(".main-container").style.width = "";
+}
+
+
+
+
+
+
 
 
 
